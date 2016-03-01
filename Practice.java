4044984898502d888a9db52ae970077230e955eb
kkh5184 <kieuthihuynh22@gmail.com>
2016-02-29 21:28:31 -0600

    Added files via upload

---
+++ Practice.java
@@ -0,0 +1,12 @@
+public class Practice {
+    public static void main(String[] args) {
+        int num = 3110;
+        int num2 = 0;
+        int num3 = 1;
+        float num4 = 2.0f;
+        String output = "H" + num + " w" + num2 + "r" + num3 + "d "
+        + num4;
+        System.out.println(output);
+
+    }
+}
